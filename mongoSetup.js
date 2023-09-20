7ee1e8411de0637fd6fdd79fdb5f0e7e03b9295f
thanigaivel-23 <23thanigaivel@gmail.com>
2023-09-20 17:05:52 +0530

    Update mongoSetup.js

--- mongoSetup.js
+++ mongoSetup.js
@@ -1,10 +1,14 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose')
-
-const connectDB = mongoose.connect("mongodb://127.0.0.1:27017/tution").then(()=>{
-    console.log("Database wokring")
-}).catch(()=>{
-    console.log("not working")
-})
+const connectDB = mongoose
+  .connect(
+    "mongodb+srv://vellesnar:velambrose@cluster0.bk4fzut.mongodb.net/crud"
+  )
+  .then(() => {
+    console.log("Database wokring");
+  })
+  .catch(() => {
+    console.log("not working");
+  });
 
 module.exports = connectDB;
